9643422622b9bf28d0812d7617ab54e9db9d7268
ps1298 <sanakapremsai@gmail.com>
2026-05-17 03:04:14 -0400

    Completed unit testing

---
+++ test_emotion_detection.py
@@ -0,0 +1,22 @@
+from EmotionDetection import emotion_detector
+
+def test_emotion_detector():
+
+    test_cases = {
+        "I am glad this happened": "joy",
+        "I am really mad about this": "anger",
+        "I feel disgusted just hearing about this": "disgust",
+        "I am so sad about this": "sadness",
+        "I am really afraid that this will happen": "fear"
+    }
+
+    for text, expected_emotion in test_cases.items():
+
+        response = emotion_detector(text)
+
+        if response['dominant_emotion'] == expected_emotion:
+            print("Passed")
+        else:
+            print("Failed")
+
+test_emotion_detector()
